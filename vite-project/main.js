b7f5d764f9f566d5e1b0934573f3c1b50f470222
Yessir664 <justin.zhang23@sitechhs.com>
2021-12-09 10:58:08 -0500

    commit

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,3 +1,19 @@
 import './style.css'
 
-const URL = "https://dash.valorant-api.com/endpoints/agents"
+const URL = "https://valorant-api.com/v1/agents"
+
+async function getData(URL){
+    try{
+        const response = await fetch(URL);
+        const data = await response.json();
+        console.log(data);
+        return data
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+const promise = getData(URL)
+let data;
+promise.then((result) => data = result)
+console.log(data)
