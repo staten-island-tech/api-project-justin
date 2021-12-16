376e87c48899ba85047804a4a85e74e75860e643
Yessir664 <justin.zhang23@sitechhs.com>
2021-12-16 10:28:31 -0500

    commit

--- vite-project/js/functions.js
+++ vite-project/js/functions.js
@@ -1,6 +1,6 @@
 function inner_generator (agent){
     return `
-        <a class="agent-link" href="${agent.displayName.toLowerCase()}.html" id="${agent.displayName.toLowerCase()}"><img class="agent-icon" src=${agent.displayIcon} alt="${agent.displayName}"></a>
+        <a class="agent-link" href=${agent.displayName.toLowerCase()}.html id=${agent.displayName.toLowerCase()}><img class="agent-icon" src=${agent.displayIcon} alt=${agent.displayName}></a>
       `
 }
 
